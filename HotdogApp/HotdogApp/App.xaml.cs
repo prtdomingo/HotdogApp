3a7909a4485f7b21b3a955c275d016789d7f6e88
Philip Domingo <philiproimon.domingo@navitaire.com>
2017-10-18 16:04:53 +0800

    Initial Commit

---
+++ HotdogApp/HotdogApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using HotdogApp.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace HotdogApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
